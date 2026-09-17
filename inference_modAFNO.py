8c9f00283c5025e0a565c2ab1e19a55878ffc808
yungyun0721 <yungyuncheng0721@gmail.com>
2026-09-17 22:51:07 +0800

    modAFNO

--- inference_modAFNO.py
+++ inference_modAFNO.py
@@ -134,7 +134,9 @@ def main(input_folder, save_folder, IC_time,
                 ):
                     out = model(total_data, t_norm)
             # out = model(total_data, t_norm)
-            out = out.detach().numpy()
+            # backto CPU
+            out = out.float().cpu().numpy()
+            del total_data, t_norm
             out *= input_scale
             out += input_center    
             np.save(os.path.join(save_folder, f'output_weather_{target_time:0>3}h'),out.squeeze())
